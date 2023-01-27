221572ff1ffdf09cd6c86d4c24967677fd51700c
imranshaiksvtm <imranshaiksvtm013@gmail.com>
2023-01-28 00:06:17 +0530

    addded hello world

--- 2-Variables.sh
+++ 2-Variables.sh
@@ -19,9 +19,24 @@ mystate=Andhrapradesh
 mydistric=Annamayys
 mymandal=Madanapalle
 
-
+echo My Country name is   : $mycountry
+echo my sate name is      : $mystate
+echo my distric name is   : $mydistric
+echo my mandal name is    : $mymandal
 
       echo -e "\e[31mMy Country name is :  $mycountry\E[0m"
       echo -e "\e[32mmy sate name is    :  $mystate\E[0m"
       echo -e "\e[33mmy distric name is :  $mydistric\E[0m"
       echo -e "\e[34mmy mandal name is  :  $mymandal\E[0m"
+
+
+echo My Country name is   :   echo -e "\e[31m$mycountry\E[0m"
+echo my sate name is      :   echo -e "\e[32m $mystate\E[0m"
+echo my distric name is   :   echo -e "\e[33m$mydistric\E[0m"
+echo my mandal name is    :   echo -e "\e[34m $mymandal\E[0m"
+
+
+
+
+
+
