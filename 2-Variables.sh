ee8225d76485fb3af131accc372c63d82865942a
imranshaiksvtm <imranshaiksvtm013@gmail.com>
2023-01-27 23:27:19 +0530

    addded hello world

--- 2-Variables.sh
+++ 2-Variables.sh
@@ -1,7 +1,14 @@
 myname=ShaikImran
 myqualification=BTech
 
+myschool=ZPHS
+x=15
+y=10
+
 
 echo My Name is :  $myname
 
 echo My Qualification is : $myqualification
+
+
+echo Addition of x and y : $x+$y
