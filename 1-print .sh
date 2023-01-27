31910981dcbe92d91308da3ad08ebd80b46c0d6e
imranshaiksvtm <imranshaiksvtm013@gmail.com>
2023-01-27 11:46:59 +0530

    addded hello world

--- 1-print .sh
+++ 1-print .sh
@@ -4,21 +4,18 @@ echo "***** Hello imran shaik ****"
 
 
 # color code syntax  : echo -e "\e[ COLmMESSAGGE\E[0m"
-# Red             - 31 -  echo -e "\e[ COLmMESSAGGE\e[0m"
-# Green           - 32 -  echo -e "\e[ COLmMESSAGGE\e[0m"
-# Yellow          - 33 -  echo -e "\e[ COLmMESSAGGE\e[0m"
-# Blue            - 34 -  echo -e "\e[ COLmMESSAGGE\e[0m"
-# Magenta         - 35 -  echo -e "\e[ COLmMESSAGGE\e[0m"
-# cyan            - 36 -  echo -e "\e[ COLmMESSAGGE\e[0m"
+# Red             - 31 -  echo -e "\e[ 31mMESSAGGE\e[0m"
+# Green           - 32 -  echo -e "\e[ 32mMESSAGGE\e[0m"
+# Yellow          - 33 -  echo -e "\e[ 33mMESSAGGE\e[0m"
+# Blue            - 34 -  echo -e "\e[ 34mMESSAGGE\e[0m"
+# Magenta         - 35 -  echo -e "\e[ 35mMESSAGGE\e[0m"
+# cyan            - 36 -  echo -e "\e[ 36mMESSAGGE\e[0m"
 
 
 
-  echo -e "\e[ 31mMESSAGGE\E[0m"
-  echo -e "\e[ 32mMESSAGGE\E[0m"
-  echo -e "\e[ 33mMESSAGGE\E[0m"
-  echo -e "\e[ 34mMESSAGGE\E[0m"
-  echo -e "\e[ 35mMESSAGGE\E[0m"
-  echo -e "\e[ 36mMESSAGGE\E[0m"
-
-
-
+      echo -e "\e[ 31mMESSAGGE\E[0m"
+      echo -e "\e[ 32mMESSAGGE\E[0m"
+      echo -e "\e[ 33mMESSAGGE\E[0m"
+      echo -e "\e[ 34mMESSAGGE\E[0m"
+      echo -e "\e[ 35mMESSAGGE\E[0m"
+      echo -e "\e[ 36mMESSAGGE\E[0m"
