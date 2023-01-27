2839132b4e94ea9b20701f80f6c08ddbd03699a2
imranshaiksvtm <imranshaiksvtm013@gmail.com>
2023-01-27 11:36:01 +0530

    addded hello world

--- 1-print .sh
+++ 1-print .sh
@@ -1,3 +1,24 @@
 echo Hello world
 
 echo "***** Hello imran shaik ****"
+
+
+# color code syntax  : echo -e "\e[ COLmMESSAGGE\E[0m"
+# Red             - 31 -  echo -e "\e[ COLmMESSAGGE\e[0m"
+# Green           - 32 -  echo -e "\e[ COLmMESSAGGE\e[0m"
+# Blue            - 33 -  echo -e "\e[ COLmMESSAGGE\e[0m"
+# Yellow          - 34 -  echo -e "\e[ COLmMESSAGGE\e[0m"
+# Magenta         - 35 -  echo -e "\e[ COLmMESSAGGE\e[0m"
+# cyan            - 36 -  echo -e "\e[ COLmMESSAGGE\e[0m"
+
+
+
+  echo -e "\e[ 31mMESSAGGE\E[0m"
+  echo -e "\e[ 32mMESSAGGE\E[0m"
+  echo -e "\e[ 33mMESSAGGE\E[0m"
+  echo -e "\e[ 34mMESSAGGE\E[0m"
+  echo -e "\e[ 35mMESSAGGE\E[0m"
+  echo -e "\e[ 36mMESSAGGE\E[0m"
+
+
+
