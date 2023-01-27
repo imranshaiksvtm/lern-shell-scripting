41a0eec5e0aa50bc9770060c408a3a136b833383
imranshaiksvtm <imranshaiksvtm013@gmail.com>
2023-01-28 00:07:59 +0530

    addded hello world

--- 2-Variables.sh
+++ 2-Variables.sh
@@ -30,13 +30,8 @@ echo my mandal name is    : $mymandal
       echo -e "\e[34mmy mandal name is  :  $mymandal\E[0m"
 
 
-echo My Country name is   :   echo -e "\e[31m$mycountry\E[0m"
-echo my sate name is      :   echo -e "\e[32m $mystate\E[0m"
-echo my distric name is   :   echo -e "\e[33m$mydistric\E[0m"
-echo my mandal name is    :   echo -e "\e[34m $mymandal\E[0m"
-
-
-
-
-
+echo My Country name is   : $mycountry
+echo my sate name is      : $mystate
+echo my distric name is   : $mydistric
+echo my mandal name is    : $mymandal
 
